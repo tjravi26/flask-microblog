b96306bcda9244908e52f51cdd93dfdbebd4323e
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-09 20:25:42 +0530

    Imported render_template

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app_package import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Ravi'}
+    return render_template('index.html', title='Home', user=user)
