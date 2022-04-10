ed50304afa576b183c9bb6762e204ceaa0d87669
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-10 16:49:38 +0530

    Created a class Config and imported it.

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+# Created a Config class in /config.py and imported it.
+from config import Config
+
 app = Flask(__name__)
+
+# Config class which contains flask configurations.
+app.config.from_object(Config)
 
 from app_package import routes  # noqa: E402, F401
