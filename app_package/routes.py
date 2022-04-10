b6620b5e9dc6f8363a75c1b6a322401bf7aba516
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-10 18:36:47 +0530

    Added login button.

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app_package import app
+from app_package.forms import LoginForm
 
 
 @app.route('/')
@@ -17,3 +18,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
