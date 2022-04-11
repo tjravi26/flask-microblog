c73ec2d1e7c2c0fa4a08991883143804359a24e2
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-11 20:08:12 +0530

    Imported and configured flask-migrate

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 # Created a Config class in /config.py and imported it.
 from config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # Config class which contains flask configurations.
 app.config.from_object(Config)
 
-from app_package import routes  # noqa: E402, F401
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app_package import routes, models  # noqa: E402, F401
