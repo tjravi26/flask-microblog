0de3d668dd017a29677ba3614642790050d5cd9a
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-11 20:07:32 +0530

    Added databse url

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'potato'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
