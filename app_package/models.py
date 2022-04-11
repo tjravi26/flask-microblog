d1573105ebc27e807df6d59894757e7457ab9245
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-11 23:39:16 +0530

    Minor changes in User model

--- app_package/models.py
+++ app_package/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Posts', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"User: {self.username}"
