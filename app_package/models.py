2b4c40131b7858b1e50fbc7745bfcc729e6f0c8d
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-11 23:32:50 +0530

    Renamed Posts model to Post

--- app_package/models.py
+++ app_package/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
         return f"User: {self.username}"
 
 
-class Posts(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True,
