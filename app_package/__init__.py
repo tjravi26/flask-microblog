69525d62f9bc9e004ceec29a38001f6842a05448
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-13 17:36:45 +0530

    Added LoginManager functionality

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -3,8 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 # Created a Config class in /config.py and imported it.
 from config import Config
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# Used to remember user login session.
+login = LoginManager(app)
+
+# Forces users to login. Requires the 'login' view function as input.
+login.login_view = 'login'
 
 # Config class which contains flask configurations.
 app.config.from_object(Config)
