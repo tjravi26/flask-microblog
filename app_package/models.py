1a702751beb2d3ac46c27cb800a34034719c0253
Ravi Teja <32415302+tjravi26@users.noreply.github.com>
2022-04-22 14:26:27 +0530

    Added user avatars

--- app_package/models.py
+++ app_package/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app_package import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5  # To add avatars to user profiles.
 
 
 class User(UserMixin, db.Model):
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"User: {self.username}"
@@ -19,6 +22,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):  # For adding avatars to user profiles.
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
 
 class Post(db.Model):
